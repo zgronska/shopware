4981f00a6760c803683625b00ff6ba1a97339690
Nicolas Fortier <n.fortier@shopware.com>
2024-10-29 15:24:57 +0100

    NEXT-35068 - reshape test

--- tests/unit/Core/Framework/RateLimiter/Policy/RateLimiterFactoryTest.php
+++ tests/unit/Core/Framework/RateLimiter/Policy/RateLimiterFactoryTest.php
@@ -1,14 +1,14 @@
 <?php declare(strict_types=1);
 
-namespace Shopware\Tests\Integration\Core\Framework\RateLimiter\Policy;
+namespace Shopware\Tests\Unit\Core\Framework\RateLimiter\Policy;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Framework\RateLimiter\Policy\TimeBackoffLimiter;
 use Shopware\Core\Framework\RateLimiter\RateLimiterFactory;
-use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\RateLimiter\Policy\SlidingWindowLimiter;
 use Symfony\Component\RateLimiter\Policy\TokenBucketLimiter;
 use Symfony\Component\RateLimiter\Storage\StorageInterface;
 
@@ -18,8 +18,6 @@ use Symfony\Component\RateLimiter\Storage\StorageInterface;
 #[CoversClass(RateLimiterFactory::class)]
 class RateLimiterFactoryTest extends TestCase
 {
-    use IntegrationTestBehaviour;
-
     public function testFactoryShouldReturnCustomPolicy(): void
     {
         $factory = new RateLimiterFactory(
@@ -92,5 +90,23 @@ class RateLimiterFactoryTest extends TestCase
         );
 
         static::assertInstanceOf(TokenBucketLimiter::class, $factory->create('example'));
+    }
+
+    public function testFactoryShouldKeepConfigUntouched(): void
+    {
+        $factory = new RateLimiterFactory(
+            [
+                'enabled' => true,
+                'id' => 'test_limiter',
+                'policy' => 'sliding_window',
+                'limit' => 1,
+                'interval' => '15 seconds',
+            ],
+            $this->createMock(StorageInterface::class),
+            $this->createMock(SystemConfigService::class),
+            $this->createMock(LockFactory::class),
+        );
+        static::assertInstanceOf(SlidingWindowLimiter::class, $factory->create('example_1'));
+        static::assertInstanceOf(SlidingWindowLimiter::class, $factory->create('example_2'));
     }
 }
