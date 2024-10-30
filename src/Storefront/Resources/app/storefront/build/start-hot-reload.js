60c12c2dc4fd0c9a540c17ca467c2db68b33a510
Björn Meyer <b.meyer@shopware.com>
2024-10-30 13:03:12 +0000

    NEXT-39321 - Set cookies and follow redirects for HOT reloading

--- src/Storefront/Resources/app/storefront/build/start-hot-reload.js
+++ src/Storefront/Resources/app/storefront/build/start-hot-reload.js
@@ -1,6 +1,7 @@
 /* eslint no-console: 0 */
-const httpProxy = require('http-proxy');
-const nodeServer = require('node:http');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const nodeServerHttp = require('node:http');
+const nodeServerHttps = require('node:https');
 const fs = require('node:fs');
 const path = require('node:path');
 const { spawn } = require('node:child_process');
@@ -31,8 +32,83 @@ const proxyOptions = {
     proxyHost: proxyUrlEnv.host,
     proxyPort: proxyPort,
     secure: appUrlEnv.protocol === 'https:' && sslFilesFound,
-    selfHandleResponse : true,
+    selfHandleResponse: true,
     target: appUrlEnv.origin,
+    autoRewrite: true,
+    followRedirects: true,
+    changeOrigin: true,
+    headers: {
+        host: appUrlEnv.host,
+        'hot-reload-mode': 'true',
+        'accept-encoding': 'identity',
+    },
+    cookieDomainRewrite: {
+        '*': '',
+    },
+    cookiePathRewrite: {
+        '*': '',
+    },
+    on: {
+        proxyReq: (proxyReq, req) => {
+            // Hot reload updates
+            if (req.url.indexOf('/sockjs-node/') === 0 || req.url.indexOf('hot-update.json') !== -1 || req.url.indexOf('hot-update.js') !== -1) {
+                proxyReq.host = '127.0.0.1';
+                proxyReq.port = assetPort;
+            }
+        },
+        proxyRes: (proxyRes, req, res) => {
+            let body = [];
+            // Set the cookie from the proxy response to the next proxy request
+            if (proxyRes.headers['set-cookie']) {
+                res.setHeader('set-cookie', proxyRes.headers['set-cookie']);
+            }
+            proxyRes.on('data', (chunk) => {
+                body.push(chunk);
+            });
+            proxyRes.on('end', () => {
+                body = Buffer.concat(body).toString();
+                body = body
+                    // replace the webpack hot proxy with the url of the live reload server
+                    .replace(new RegExp('/_webpack_hot_proxy_/', 'g'), `${proxyUrlEnv.protocol}//${proxyUrlEnv.hostname}:${assetPort}/`)
+                    // replace the domain without port or without port with the proxy url
+                    .replace(new RegExp(`${appUrlEnv.origin}/`, 'g'), `${proxyUrlEnv.origin}/`)
+                    // replace the media url back to use the default storefront url
+                    .replace(new RegExp(`${proxyUrlEnv.origin}/media/`, 'g'), `${appUrlEnv.origin}/media/`)
+                    // replace the thumbnail url back to use the default storefront url
+                    .replace(new RegExp(`${proxyUrlEnv.origin}/thumbnail/`, 'g'), `${appUrlEnv.origin}/thumbnail/`)
+                    // Replace Symfony Profiler URL to relative url @see: https://regex101.com/r/HMQd2n/2
+                    .replace(/http[s]?\\u003A\\\/\\\/[\w.]*(:\d*|\\u003A\d*)?\\\/_wdt/gm, '/_wdt')
+                    .replace(/new\s*URL\(url\);\s*url\.searchParams\.set\('XDEBUG_IGNORE'/gm, 'new URL(window.location.protocol+\'//\'+window.location.host+url);                url.searchParams.set(\'XDEBUG_IGNORE\'');
+                res.end(body);
+            });
+        },
+        error: (err, req, res) => {
+            console.error(err);
+
+            if (err.code === 'UNABLE_TO_GET_ISSUER_CERT_LOCALLY') {
+                console.error('Make sure that node.js trusts the provided certificate. Set NODE_EXTRA_CA_CERTS for this.');
+                console.error(`Try to start again with NODE_EXTRA_CA_CERTS="${certPath}" set.`);
+                process.exit(1);
+            }
+
+            if (err.code === 'SSL_ERROR_NO_CYPHER_OVERLAP') {
+                console.error('Try to start watcher again with specific path to https (key and crt) files like this:');
+                console.error('STOREFRONT_HTTPS_KEY_FILE=/var/www/html/.../certs/shopware.key STOREFRONT_HTTPS_CERTIFICATE_FILE=/var/www/html/../certs/shopware.crt composer run watch:storefront');
+                process.exit(1);
+            }
+
+            if (err.code === 'ENOTFOUND') {
+                console.error('The domain could not be resolved. Make sure that the domain is correct in DEVENV/DDEV.');
+                console.error('And if this is a custom domain, make sure that the domain is set in your /etc/hosts file.');
+                process.exit(1);
+            }
+
+            res.writeHead(500, {
+                'Content-Type': 'text/plain',
+            });
+            res.end('Something went wrong. Check the console for more information.');
+        },
+    },
 };
 
 if (appUrlEnv.protocol === 'https:' && !sslFilesFound) {
@@ -41,93 +117,38 @@ if (appUrlEnv.protocol === 'https:' && !sslFilesFound) {
     console.error('If you use a TLS proxy (like in DDEV Shopware 6 setup), you can ignore this message.');
 }
 
-const sslOptions = appUrlEnv.protocol === 'https:' && sslFilesFound ? {
+const sslOptions = proxyUrlEnv.protocol === 'https:' ? {
     key: fs.readFileSync(keyPath),
     cert: fs.readFileSync(certPath),
 } : {};
 
-const proxyServerOptions = Object.assign(proxyOptions,  { ssl: sslOptions });
-
-const proxy = httpProxy.createServer(proxyServerOptions);
-proxy.on('error', (err, req, res) => {
-    console.error(err);
-
-    if (err.code === 'UNABLE_TO_GET_ISSUER_CERT_LOCALLY') {
-        console.error('Make sure that node.js trusts the provided certificate. Set NODE_EXTRA_CA_CERTS for this.');
-        console.error(`Try to start again with NODE_EXTRA_CA_CERTS="${certPath}" set.`);
-        process.exit(1);
-    }
-
-    if (err.code === 'ENOTFOUND') {
-        console.error('The domain could not be resolved. Make sure that the domain is correct in DEVENV/DDEV.');
-        console.error('And if this is a custom domain, make sure that the domain is set in your /etc/hosts file.');
-        process.exit(1);
-    }
-
-    res.writeHead(500, {
-        'Content-Type': 'text/plain',
-    });
-    res.end('Something went wrong. Check the console for more information.');
+const proxyServerOptions = Object.assign(proxyOptions, { ssl: sslOptions });
+const proxy = createProxyMiddleware(proxyServerOptions);
+const server = createLiveReloadServer(sslOptions).catch((e) => {
+    console.error(e);
+    console.error('Could not start the live server with the provided certificate files, falling back to http server.');
+    return createLiveReloadServer({});
 });
-
-proxy.on('proxyReq', (proxyReq, req) => {
-    proxyReq.setHeader('host', appUrlEnv.host);
-    proxyReq.setHeader('hot-reload-mode', 'true');
-    proxyReq.setHeader('accept-encoding', 'identity');
-
-    // Hot reload updates
-    if (req.url.indexOf('/sockjs-node/') === 0 || req.url.indexOf('hot-update.json') !== -1 || req.url.indexOf('hot-update.js') !== -1) {
-        proxyReq.host = '127.0.0.1';
-        proxyReq.port = assetPort;
-    }
-});
-
-proxy.on('proxyRes', (proxyRes, req, res) => {
-    let body = [];
-    proxyRes.on('data', (chunk) => {
-        body.push(chunk);
-    });
-    proxyRes.on('end', () => {
-        body = Buffer.concat(body).toString();
-        body = body
-            // replace the webpack hot proxy with the url of the live reload server
-            .replace(new RegExp('/_webpack_hot_proxy_/', 'g'), `${proxyUrlEnv.protocol}//${proxyUrlEnv.hostname}:${assetPort}/`)
-            // replace the domain without port or without port with the proxy url
-            .replace(new RegExp(`${appUrlEnv.origin}/`, 'g'), `${proxyUrlEnv.origin}/`)
-            // replace the media url back to use the default storefront url
-            .replace(new RegExp(`${proxyUrlEnv.origin}/media/`, 'g'), `${appUrlEnv.origin}/media/`)
-            // replace the thumbnail url back to use the default storefront url
-            .replace(new RegExp(`${proxyUrlEnv.origin}/thumbnail/`, 'g'), `${appUrlEnv.origin}/thumbnail/`)
-            // Replace Symfony Profiler URL to relative url @see: https://regex101.com/r/HMQd2n/2
-            .replace(/http[s]?\\u003A\\\/\\\/[\w.]*(:\d*|\\u003A\d*)?\\\/_wdt/gm, '/_wdt')
-            .replace(/new\s*URL\(url\);\s*url\.searchParams\.set\('XDEBUG_IGNORE'/gm, 'new URL(window.location.protocol+\'//\'+window.location.host+url);                url.searchParams.set(\'XDEBUG_IGNORE\'');
-        res.end(body);
-    });
-});
-
-const server = createLiveReloadServer(sslOptions);
 server.then(() => {
     console.log('############');
     console.log(`Default TWIG Storefront: ${appUrlEnv.origin}`);
     console.log(`Proxy server hot reload: ${proxyUrlEnv.origin}`);
     console.log('############');
 
-    if (appUrlEnv.protocol === 'http:' || appUrlEnv.protocol === 'https:' && !sslFilesFound) {
-        if (appUrlEnv.protocol === 'http:') {
-            console.log('Proxy uses the http schema.');
+    if (proxyUrlEnv.protocol === 'https:') {
+        try {
+            nodeServerHttps.createServer(sslOptions, proxy).listen(proxyPort);
+            console.log('Proxy uses the https schema, with ssl certificate files.');
+        } catch (e) {
+            console.error(e);
+            console.error('Could not start the proxy server with the provided certificate files, falling back to http server.');
+            proxyUrlEnv.protocol = 'http:';
         }
-        if (appUrlEnv.protocol === 'https:' && !sslFilesFound) {
-            console.log('Proxy uses the https schema, without ssl certificate files.');
-            console.log('Content is bypassed via a node http server (Case: TLS proxy in front).');
-        }
-        nodeServer.createServer((req, res) => {
-            proxy.web(req, res);
-        }).listen(proxyPort);
     }
 
-    if (appUrlEnv.protocol === 'https:' && sslFilesFound) {
-        console.log('Proxy uses the https schema, with ssl certificate files.');
-        proxy.listen(proxyServerOptions.proxyPort);
+    if (proxyUrlEnv.protocol === 'http:') {
+        console.log('Proxy uses the http schema.');
+        nodeServerHttp.createServer(proxy).listen(proxyPort);
     }
 
     console.log('############');
